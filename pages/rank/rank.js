203cd7daea6d9056a46aca521e8888ab94511df3
Linlin Yan <yanlinlin82@gmail.com>
2023-09-24 01:08:54 +0800

    Show rank lists.

--- pages/rank/rank.js
+++ pages/rank/rank.js
@@ -1,66 +1,21 @@
-// pages/rank.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    currentTab: 0,
+    tabs: ["本月榜单", "上月榜单", "总榜单", "杂志榜单"],
+    tables: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  onLoad: function () {
+    console.log('rank - onLoad')
+    wx.request({
+      url: 'https://paper-hub.cn/api/fetch_rank_list',
+      success: (res) => {
+        console.log(res)
+        this.setData({tables: res.data.results})
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  switchTab: function (event) {
+    const index = event.currentTarget.dataset.index;
+    this.setData({ currentTab: index })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
-})
+});
