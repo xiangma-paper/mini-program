9884527e4a86761a936d4d95dd2538b36e339714
Linlin Yan <linlin.yan@bioinfo.app>
2024-01-06 01:29:27 +0800

    Show mini program version.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 App({
+  version: '0.1.3',
   host: 'https://paper-hub.cn',
   host_debug: 'https://paper-hub.cn:8443',
   token: '',
--- pages/about/about.js
+++ pages/about/about.js
@@ -1,5 +1,11 @@
+const app = getApp()
+
 Page({
+  data: {
+    version: ''
+  },
   onLoad: function() {
     console.log('about.onLoad()')
+    this.setData({version: app.version})
   }
 })
